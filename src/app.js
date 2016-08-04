9715489a865d42551a92e1ad270b1c8cd60d28b5
Keily Salinas <ksalinas@socialh4ck.com>
2016-08-04 12:49:45 -0400

    Changes Added index Added app.js Added routes

---
+++ src/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('app', ['ui.router','app.controller', 'app.services', 'app.dashboard', 'app.news'])
+
+angular
+  .module('app')
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        views: {
+          '': { templateUrl: './templates/base/app.html', controller: 'appController' },
+          'dashboard@home': { templateUrl: './templates/dashboard/dashboard_tpl.html', controller: 'DahsboardController' },
+          'news@home': { templateUrl: './templates/news/news_tpl.html', controller: 'NewsController' }
+        }
+      });
+
+    $urlRouterProvider.otherwise('/');
+  }]);
+
+angular.module('app.controller', [])
+  .controller('appController', ['$scope', '$state', function($scope, $state) {
+
+    $scope.currentView = 'dashboard';
+
+    $scope.templates = {
+      header: 'templates/base/header_tpl.html',
+      aside: 'templates/base/aside_tpl.html'
+    };
+
+    $scope.changeView = function(view) {
+      $scope.currentView = view;
+    };
+
+  }]);
