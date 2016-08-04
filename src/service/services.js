2659da3671d9565b315fde00adb9df6196458ca1
Keily Salinas <keisalinas2@gmail.com>
2016-08-04 14:56:10 -0400

    get data new service

--- src/service/services.js
+++ src/service/services.js
@@ -5,3 +5,8 @@ angular.module('app.services', [])
   .service('lodash', ['$window', function($window) {
     return $window._;
   }])
+   .factory('loadFile', ['$http', function($http) {
+    return function loadFile() {
+      return $http.get('https://dl.dropboxusercontent.com/u/79679734/data.json');
+    };
+  }])
